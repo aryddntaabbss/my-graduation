1df041556e7c5134e851bfe91e9e5462cce64cd9
Aryaddinata Abbas <aryaddinataabbas017@gmail.com>
2026-03-13 04:04:59 +0900

    Add migration for guests table with necessary fields

---
+++ database/migrations/2026_03_12_190400_create_guests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->boolean('attendance')->nullable();
+            $table->integer('guest_count')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guests');
+    }
+};
